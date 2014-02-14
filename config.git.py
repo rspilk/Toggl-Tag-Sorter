2a9c4f2e190e3d214bf8fe117c5fa98e259721a2
tspilk <tspilker@zagmail.gonzaga.edu>
2014-02-14 09:24:39 -0800

    Completely rewrote and added a config file

---
+++ config.git.py
@@ -0,0 +1,9 @@
+
+# Toggl API Token
+API_TOKEN =     'ENTER API TOKEN'
+API_PASSWORD =  'api_token'
+WORKSPACE =     'ENTER WORKSPACE NUM' 
+UID       =     'EMTER UID NUM'
+
+USER_AGENT =    'PUT A NAME AND YOUR EMAIL HERE NO SPACES'
+URL_BASE   =    'https://toggl.com/reports/api/v2/details?'
