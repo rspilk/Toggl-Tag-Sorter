9429874b2975e990c285ab0de2bdad68c2a33e51
Tyler Spilker <spilkert@its.gonzaga.edu>
2014-02-14 12:46:21 -0800

    Typo in message

--- config.git.py
+++ config.git.py
@@ -3,7 +3,7 @@
 API_TOKEN =     'ENTER API TOKEN'
 API_PASSWORD =  'api_token'
 WORKSPACE =     'ENTER WORKSPACE NUM' 
-UID       =     'EMTER UID NUM'
+UID       =     'ENTER UID NUM'
 
 USER_AGENT =    'PUT A NAME AND YOUR EMAIL HERE NO SPACES'
 URL_BASE   =    'https://toggl.com/reports/api/v2/details?'
